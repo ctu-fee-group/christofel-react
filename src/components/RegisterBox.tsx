6ecc4bc8e2ed80ab42da326432b44790be51d2cf
František Boháček <fandabohacek@gmail.com>
2022-04-29 19:32:39 +0200

    feat: move auth success to the bottom of the box

--- src/components/RegisterBox.tsx
+++ src/components/RegisterBox.tsx
@@ -1,62 +1,78 @@
-import {Component} from 'react';
+import { Component } from 'react';
 import styles from './Box.module.css';
-import Box from "./Box";
-import Button from "./Button";
+import Box from './Box';
+import Button from './Button';
 
 interface RegisterBoxProps {
-    code?: string
-    discordSuccess: boolean;
-    ctuSuccess: boolean;
+  code?: string;
+  discordSuccess: boolean;
+  ctuSuccess: boolean;
 }
 
 class RegisterBox extends Component<RegisterBoxProps> {
-    static defaultProps = {
-        code: undefined
+  static defaultProps = {
+    code: undefined,
+  };
+
+  private static createCodeUrl(
+    url: string | undefined,
+    code?: string | undefined
+  ): string {
+    if (!url) {
+      return 'url not set';
     }
 
-    private static createCodeUrl(url: string|undefined, code?: string|undefined) : string
-    {
-        if (!url){
-            return "url not set";
-        }
-
-        try {
-            const urlObj = new URL(url);
-            if (code != null) {
-                urlObj.searchParams.append('state', code);
-            }
-            return urlObj.toString();
-        }
-        catch {
-            return "could not generate url";
-        }
+    try {
+      const urlObj = new URL(url);
+      if (code != null) {
+        urlObj.searchParams.append('state', code);
+      }
+      return urlObj.toString();
+    } catch {
+      return 'could not generate url';
     }
+  }
 
-    render() {
-        const {code, discordSuccess, ctuSuccess} = this.props;
-        const discordOauthUrl = RegisterBox.createCodeUrl(process.env.oauthDiscordUrl, code);
-        const ctuOauthUrl = RegisterBox.createCodeUrl(process.env.oauthCtuUrl, code);
+  render() {
+    const { code, discordSuccess, ctuSuccess } = this.props;
+    const discordOauthUrl = RegisterBox.createCodeUrl(
+      process.env.oauthDiscordUrl,
+      code
+    );
+    const ctuOauthUrl = RegisterBox.createCodeUrl(
+      process.env.oauthCtuUrl,
+      code
+    );
 
-        return (
-            <Box title="FEL" subtitle="Discord server">
-                <div className={styles.button_box}>
-                    {ctuSuccess && discordSuccess && <h2>Nyní můžeš okno zavřít a vrátit se na server.</h2>}
-                </div>
+    return (
+      <Box title="FEL" subtitle="Discord server">
+        <div className={styles.button_box}>
+          <h2>Krok 1.</h2>
+          <Button
+            href={discordOauthUrl}
+            success={discordSuccess}
+            disabled={false}
+          >
+            Discord přihlášení
+          </Button>
 
-                <div className={styles.button_box}>
-                    <h2>Krok 1.</h2>
-                    <Button href={discordOauthUrl} success={discordSuccess} disabled={false}>
-                        Discord přihlášení
-                    </Button>
+          <h2>Krok 2.</h2>
+          <Button
+            href={ctuOauthUrl}
+            success={ctuSuccess}
+            disabled={!discordSuccess}
+          >
+            ČVUT přihlášení
+          </Button>
 
-                    <h2>Krok 2.</h2>
-                    <Button href={ctuOauthUrl} success={ctuSuccess} disabled={!discordSuccess}>
-                        ČVUT přihlášení
-                    </Button>
-                </div>
-            </Box>
-        );
-    }
+          <h2>Úspěšné ověření</h2>
+          {ctuSuccess && discordSuccess && (
+            <h2>Nyní můžeš okno zavřít a vrátit se na server.</h2>
+          )}
+        </div>
+      </Box>
+    );
+  }
 }
 
 export default RegisterBox;
